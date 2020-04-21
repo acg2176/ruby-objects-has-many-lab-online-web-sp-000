e81ef0e0f76d2cacce8e0297a96aa194cb80d644
Anna Guerrero <acg2176@columbia.edu>
2020-04-21 21:14:17 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,20 @@
+class Post
+  :post
+  :post2
+  @@all = []
+
+  def initialize(title)
+    @title = title
+  end
+
+  def title
+    @title
+  end
+
+  def all
+
+  def self.all
+    @@all
+  end
+
+end
