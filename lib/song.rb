df3bdb6df855618498dd9d55425172e9b9f77eec
Anna Guerrero <acg2176@columbia.edu>
2020-04-21 20:08:20 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,29 @@
+class Song
+  attr_accessor :name, :artist
+  :song
+  :song2
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self     #pushes instances upon initialization
+  end
+
+  def self.all
+    @@all
+  end
+
+  def all
+    @@all
+  end
+
+  def name
+    @name
+  end
+
+  def artist
+    @artist
+  end
+
+end
