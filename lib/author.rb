0245c107380bbeca498933cb997e22ac3775d508
Anna Guerrero <acg2176@columbia.edu>
2020-04-21 21:46:28 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,6 +2,8 @@ class Author
   :betty
   attr_accessor :name
 
+  @@post_count = 0
+
   def initialize(name)
     @name= name
     @posts = []
@@ -14,6 +16,17 @@ class Author
   def add_post(post)
     @posts << post
     post.author = self
+    @@post_count += 1
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    add_post(post)
+    @@post_count += 1
+  end
+
+  def self.post_count
+    @@post_count
   end
 
 
